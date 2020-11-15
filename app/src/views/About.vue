b5c0f2b399ea328ba265fd208fffaa30d69f4e96
macros <redial@abv.bg>
2020-11-15 23:26:11 +0200

    Added brief about page

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about mt-4">
+    <b-container>
+      <div>
+        <b-card bg-variant="dark" text-variant="white" title="GPX Track">
+          <b-card-text>
+            Ако не искате да се забатачите нейде по баирите и да няма после
+            "Роска къде съм" сме ви приготвили трак който да заредите на вашите
+            навигационни устройства.
+          </b-card-text>
+          <b-button href="#" variant="primary">Download GPX</b-button>
+        </b-card>
+      </div>
+      <div class="what-is p-3">
+        <h3>Какво е Balkan Ultra?</h3>
+        <p>
+          Приключение със 100% състезателен характер! Маршрут с дължина
+          приблизително 80 км. и положителна денивелация от 6500 метра в сърцето
+          на Централна Стара планина.
+        </p>
+      </div>
+      <div class="what-is p-3">
+        <h3>Какво <strong>НЯМА</strong> на Balkan Ultra?</h3>
+        <ol>
+          <li>Стартова такса</li>
+          <li>Състезателна маркировка</li>
+          <li>Пунктове с осигурена вода и храна</li>
+          <li>Масажистки...</li>
+        </ol>
+      </div>
+      <div class="what-is p-3">
+        <h3>Какво <strong>ИМА</strong> на Balkan Ultra?</h3>
+        <ol>
+          <li>Има много Балкан за катерене;</li>
+          <li>Има времеизмерване на 7 контролни точки /5 хижи, гр. Карлово, ВЕЦ Видима/</li>
+          <li>Има официална GPS следа на маршрута</li>
+          <li>Имате 5 хижи, които може да използвате за подслон при влошаване на времето.</li>
+        </ol>
+      </div>
+    </b-container>
   </div>
 </template>
+
+<style scoped>
+.what-is {
+  background-color: rgb(235, 222, 222);
+  box-shadow: 0 2px 2px 0px rgb(107, 107, 107);
+}
+</style>
