f2d064006b3b6ea30c083bd624f86649ae13b52b
macros <redial@abv.bg>
2020-11-15 10:41:21 +0200

    Added hover styling to navbar items

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -2,26 +2,35 @@
   <b-navbar toggleable="lg" type="dark" variant="dark">
     <b-container fluid="sm" class="d-flex justify-content-center">
       <b-row>
-        <b-col>
-          <router-link to="/">
-            <b-navbar-brand>Home</b-navbar-brand>
-          </router-link>
-        </b-col>
-        <b-col>
-          <router-link to="/Register">
-            <b-navbar-brand>Register</b-navbar-brand>
-          </router-link>
-        </b-col>
-        <b-col>
-          <router-link to="/about">
-            <b-navbar-brand>Race Info</b-navbar-brand>
-          </router-link>
-        </b-col>
-        <b-col>
-          <router-link to="/about">
-            <b-navbar-brand>Results</b-navbar-brand>
-          </router-link>
-        </b-col>
+        <div class="nav-item">
+          <b-col>
+            <router-link to="/">
+              <b-navbar-brand>Home</b-navbar-brand>
+            </router-link>
+          </b-col>
+        </div>
+
+        <div class="nav-item">
+          <b-col>
+            <router-link to="/Register">
+              <b-navbar-brand>Register</b-navbar-brand>
+            </router-link>
+          </b-col>
+        </div>
+        <div class="nav-item">
+          <b-col>
+            <router-link to="/about">
+              <b-navbar-brand>Race Info</b-navbar-brand>
+            </router-link>
+          </b-col>
+        </div>
+        <div class="nav-item">
+          <b-col>
+            <router-link to="/about">
+              <b-navbar-brand>Results</b-navbar-brand>
+            </router-link>
+          </b-col>
+        </div>
       </b-row>
     </b-container>
   </b-navbar>
@@ -33,4 +42,13 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav-item {
+  border-bottom: 3px solid transparent;
+}
+
+.nav-item:hover {
+  background-color: rgb(77, 77, 83);
+  border-bottom-color: rgb(180, 95, 15);
+}
+</style>
